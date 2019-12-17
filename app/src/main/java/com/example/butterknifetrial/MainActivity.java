9b6a99733c63ee905eff9f0ccc82025f04f0dc4a
Kunal Dalwadi <kdalwadi@accu-time.com>
2019-12-17 15:43:03 -0500

    [KD]Mapping of UI and Listener

--- app/src/main/java/com/example/butterknifetrial/MainActivity.java
+++ app/src/main/java/com/example/butterknifetrial/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.butterknifetrial;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_click)
+    Button mButtonClick;
+
+    @BindView(R.id.tv_header)
+    TextView mTextViewHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+    
+    @OnClick(R.id.btn_click)
+    public void submit(){
+        Toast.makeText(this, "Button Clicked ..... ButterKnife", Toast.LENGTH_SHORT).show();
     }
 }
